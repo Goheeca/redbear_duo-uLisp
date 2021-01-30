40228b71b57f723b317e7cb2f6eed2f94dde64bc
Goheeca <goheeca@gmail.com>
2021-01-30 11:27:23 +0100

    Wake up procedure

--- firmware/ulisp/library.lisp
+++ firmware/ulisp/library.lisp
@@ -1,5 +1,9 @@
 (pinmode 7 1)
 (defun toggle () (digitalwrite 7 (not (digitalread 7))))
 (defun cloud (data) (publish "lisp" data))
-(defun alarm () #|(print (now))|#)
+(defun alarm () #|(print (now))|# (wake))
+(defun wake ()
+  (let ((n (now)))
+    (when (eq (caddr n) 0)
+      (print n))))
 #include "core/ulisp-lisp-library.h"
